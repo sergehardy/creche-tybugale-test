75d210e5e1d52e58d2136f56678dd4c860b2a6ae
Serge HARDY <serge.hardy@zenika.com>
2019-06-22 17:10:40 +0200

    Page salariés

---
+++ pages/l-equipe.php
@@ -0,0 +1,72 @@
+<h4>Rôle</h4>
+
+<p>L’association de parents emploie des professionnels de la petite enfance pour animer, organiser et accueillir les
+    enfants au quotidien.
+</p>
+
+<p>Depuis plusieurs années l’équipe de salariés est stable, en contrat à durée indéterminée.
+</p>
+
+<p>Elle est composée de :</p>
+<ul>
+    <li>2 éducateurs de jeunes enfants à temps plein : <strong>Martine et Loïc</strong>.</li>
+    <li>1 auxiliaire de puériculture à temps plein : <strong>Marine</strong>.</li>
+    <li>2 aides éducatrices diplômées du CAP Petite Enfance à temps plein : <strong>Maly et Nathalie</strong>.</li>
+    <li>1 cuisinier à 20h/semaine : <strong>Matthieu</strong>.</li>
+</ul>
+
+
+<h4>L’équipe</h4>
+<br>
+<img width="200px" src="/assets/images/marine.png">
+<img width="200px" src="/assets/images/loic.png">
+<img width="200px" src="/assets/images/martine.png">
+<img width="200px" src="/assets/images/maly.png">
+<img width="200px" src="/assets/images/nathalie.png">
+<img width="200px" src="/assets/images/matthieu.png">
+
+<h5>Loïc</h5>
+
+<blockquote>
+    <p>EJE depuis 2004 à la crèche. Je suis à mi-temps auprès des enfants et à mi-temps dans le bureau à effectuer des
+        tâches administratives de gestion : plannings des enfants et des permanences parents, contrats, facturations,
+        demandes de subventions, lien avec la Ville de Rennes et la CAF.
+    </p>
+</blockquote>
+
+<h5>Marine</h5>
+<blockquote>
+    <p>Auxiliaire de puériculture arrivée à la crèche en mars 2017, j'occupe la mission inscription en doublon avec
+        Martine et suis également référente pour les bébés de moins de 1 an.
+    </p>
+</blockquote>
+<h5>Maly</h5>
+
+<blockquote>
+
+    <p>Aide éducatrice de jeunes enfants, arrivée en 2010, j’occupe la mission de gestion des moyens généraux avec
+        un parent.
+    </p>
+</blockquote>
+
+<h5>Martine</h5>
+<blockquote>
+
+    <p>Educatrice de jeunes enfants arrivé à la crèche en mars 1986, ma mission est d’accueillir les nouvelles
+        familles en doublon avec <strong>Marine</strong> et un parent.
+    </p></blockquote>
+
+<h5>Nathalie</h5>
+<blockquote>
+
+    <p>Aide éducatrice de jeunes enfants, j'occupe mon poste depuis mai 2013. Ma mission est la gestion du stock de
+        matériel pédagogique et le suivi du poste ménage avec un parent.
+    </p></blockquote>
+
+<h5>Matthieu</h5>
+<blockquote>
+
+    <p>J’occupe le poste de cuisinier depuis mai 2012. Je m'occupe de l'élaboration des menus et des
+        approvisionnements en lien avec le poste nutrition.
+    </p>
+</blockquote>
