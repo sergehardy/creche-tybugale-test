fe41e6b614a3cb169c2382f8e893a52b73e21758
Serge HARDY <serge.hardy@zenika.com>
2019-06-24 23:05:30 +0200

    Au jour le jour

--- pages/au-jour-le-jour.php
+++ pages/au-jour-le-jour.php
@@ -1,66 +1,66 @@
 
-   <!--        <img class="" width="40%"  src="https://cdn.pixabay.com/photo/2017/04/05/08/18/kindergarten-2204239_960_720.jpg">-->
-<!--    <div class="paragraph-title">-->
-<!--            <span class="h4">Le&nbsp;matin</span>-->
-<!--            <span class="stripes" />-->
-<!--    </div>-->
+<h4>L’arrivée</h4>
 
-   <h4>Le matin</h4>
+<p>Il est 9h00 et j’arrive à la crèche. J’ai hâte de retrouver mes copains, copines. Dans le hall, j’accroche mon
+    manteau à côté de ma photo, j’ôte mes chaussures et enfile mes chaussons. Dans la salle d’accueil, j’entends déjà
+    les enfants qui jouent.
+</p>
+<p>
+    “Bonjour” tout le monde ! Nathalie, Martine et Marine sont là, avec quelques copains, copines qui jouent aux
+    voitures, à la cuisinette, lisent des livres… Il y a aussi le papa de Romain qui plie le linge dans un coin de la
+    pièce.
+</p>
 
+<h4>La matinée</h4>
 
-   <p class="text-justify">Arrivé à la crèche à 9h. Des copains sont déjà arrivés. Il y a le papa de xxx qui plie du linge.
-            On prend le petit jus de fruit dans la cuisine
-            On s’habille et on va se promener car c’est mon tour. Pendant ce temps les copains qui restent font du dessin</p>
-            <br/>
+<p>
+Vers 9h15, nous nous rassemblons dans la salle repas pour “boire un petit jus” de fruits, c’est l’occasion de se redire bonjour tous ensemble, de faire la météo, de chanter... bref de lancer la journée. Ce matin, je serai plutôt avec Martine sur le groupe des plus grands.
+</p>
 
+<p>
+Puis nous montons en salle de motricité : ici, c’est le paradis des grimpeurs, sauteurs, acrobates… La piscine à balles est vraiment mon espace préféré. Pendant ce temps, au rez de chaussée, Nathalie propose à chacun des petits un temps de repos en fonction de son rythme et de son heure de lever. Le papa de Romain l’assiste en surveillant les autres petits qui jouent sur le tapis ou déambulent à 4 pattes dans la salle de vie. Pour le groupe des moyens avec Marine, l’activité de ce matin, c’est jeux d’eau en salle de vie à l’étage : on les entend rigoler depuis la salle de motricité.
+</p>
 
-        <h4>Le midi</h4>
-            <p  class="text-justify">On mange . Mathieu nous a fait … C’est bon! La maman de … aide les petits à manger.</p>
-<!--            <img width="30%"  src="/assets/images/au-jour-le-jour/2.png">-->
-            <br/>
-            <h4>La sieste</h4>
-            <br/>
-            <p class="text-justify">Je vais faire dodo!
-            Quand je me réveille ma maman est là pour sa permanence!
-             <br/>
-<!--    <img width="30%"  src="/assets/images/au-jour-le-jour/3.png">-->
+<img src="/assets/images/jardinage.png" class="float-left">
+<p>
+    Il est 11h. Avant d’aller dans le jardin, nous avons fait des lotos avec Martine mais maintenant il est temps d’aller dehors voir si nos tomates et radis ont bien poussé dans le jardin. Si besoin, nous allons les arroser. Nous retrouvons le groupe des moyens et les petits qui ne dorment pas : tous ensemble, on joue aux vélos, draisiennes, bac à sable, cabanes...il y a temps de chose à faire dehors. Déjà les bonnes odeurs de la cuisine nous ouvrent l’appétit ! Matthieu a cuisiné un chili con carne qui fait gargouiller nos ventres !
+</p>
 
-    <h4>L'après-midi</h4>
+<h4>Le déjeuner</h4>
 
-            <p class="text-justify">Le goûter: compote, pain et fromage! Miam</p>
-            <p class="text-justify">On va jouer dans le jardin: les petits vont dans le bac sable, les grands font du toboggan ou du vélo sur la piste.</p>
-            <br/>
-            <p class="text-justify">Je peux rentrer chez moi!</p>
+<p>
+Il est 11h30. Nous avons ôté nos manteaux et chaussures, lavé nos mains et fait un passage aux toillettes et nous voici attablé avec les copains copines pour partager un bon repas. Matthieu cuisine pour nous de bons produits frais et bio. Nathalie, Marine, Martine, le papa de Romain et Matthieu mangent avec nous. Il y a tant de choses à dire et raconter autour d’un bon repas !
+</p>
 
-<!--        </div>-->
+<h4>La sieste</h4>
 
+<p>
+12h30 environ, je commence à être un peu fatigué moi. Le groupe des plus grands dont je fais parti monte à l’étage pour se préparer à la sieste. Dans la salle de vie à l’étage, nous nous déshabillons, rangeons nos affaires dans nos paniers. Une petite histoire nous amène doucement vers le sommeil. Dans les deux chambres, Martine et Marine restent avec nous jusqu’à ce que tout le monde dorme… et pendant que je m’abandonne au sommeil, en bas, les plus petits se lèvent de leur sieste du matin et vont manger leur bonne purée “maison”.
+</p>
 
-<!--    <div id="carouselExampleIndicators" class="d-block w-10 carousel slide" data-ride="carousel">-->
-<!--        <ol class="carousel-indicators">-->
-<!--            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>-->
-<!--            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>-->
-<!--            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
-<!--        </ol>-->
-<!---->
-<!---->
-<!--        <div class="carousel-inner">-->
-<!--            <div class="carousel-item active">-->
-<!--                <img class="d-block w-100" src="/assets/images/au-jour-le-jour/1.png" alt="First slide">-->
-<!--            </div>-->
-<!--            <div class="carousel-item">-->
-<!--                <img class="d-block w-100" src="/assets/images/au-jour-le-jour/2.png" alt="Second slide">-->
-<!--            </div>-->
-<!--            <div class="carousel-item">-->
-<!--                <img class="d-block w-100" src="/assets/images/au-jour-le-jour/3.png" alt="Third slide">-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">-->
-<!--            <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-<!--            <span class="sr-only">Previous</span>-->
-<!--        </a>-->
-<!--        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">-->
-<!--            <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-<!--            <span class="sr-only">Next</span>-->
-<!--        </a>-->
-<!--    </div>-->
+<h4>L’après-midi</h4>
+
+<img src="/assets/images/ptit-jus.png" class="float-left">
+
+<p>
+    14h-16h. Chacun à son rythme, les copains copines et moi nous réveillons tranquillement. Au réveil, je retrouve Maly qui sera notre encadrante cet après midi. En bas, il y a aussi Loïc qui s’occupera plutôt des plus petits.</p>
+
+<p>Il y a aussi la maman de Julie et... oh surprise, mais c’est ma maman qui est là aussi ! Ah oui, c’est vrai, elle est de “permanence” cet après midi à la crèche. Je comprends mieux maintenant mais au début je ne comprenais pas trop pourquoi ma maman était à la crèche : elle est là mais ce n’est pas pour venir me chercher, on reste à la crèche. Elle s’occupe des autres enfants alors au début j’étais un peu jaloux... Elle est parfois avec moi mais elle doit aussi s’occuper de la machine à laver, de la vaisselle, de préparer le goûter….
+</p>
+
+<h4>Le goûter</h4>
+
+<p>
+En parlant de goûter d’ailleurs ! Il est 16h et nous descendons manger le pain confiture et les pommes. On garde un peu de goûter pour les copains qui dorment encore.
+Nous voyons les petits qui se lèvent pour certains ou retournent à la sieste pour d’autres…
+</p>
+    <p>Pour terminer la journée, nous avons le choix : jouer dans le jardin, jouer à la pâte à modeler, rester avec le groupe des bébés (j’aime bien rester avec les petits parfois, je leur donne des jouets et je leur montre des choses des “grands”).
+</p>
+
+<h4>Le départ</h4>
+
+<p>
+Il est 18h. Maman a fini sa permanence. La journée à la crèche se termine pour moi. Au revoir les copains copines, au revoir les encadrants. A demain !
+</p>
+
+<p><em>En pratique : La crèche ouvre à 8h et ferme ses portes à 18h30<em>.<p>
--- templates/home_block.php
+++ templates/home_block.php
@@ -1,7 +1,7 @@
 <div class="col-sm-4 px-5 white">
     <div class="box bg-<?php echo $color;?>">
         <h3><?php echo $h3;?></h3>
-        <p>
+        <p class="text-center">
             <?php echo $description;?><br><br>
             <a class="btn btn-lg btn-default" href="#"><?php echo $link;?></a>
         </p>
