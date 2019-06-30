0168687bebde899b8f8032ade1149c75e15f3137
Serge HARDY <serge.hardy@zenika.com>
2019-06-30 21:03:34 +0200

    Démarche Engagement

---
+++ pages/demarche-et-valeurs.php
@@ -0,0 +1,31 @@
+<h4>Le projet éducatif</h4>
+<img src="/assets/images/bio.png"  class="float-left">
+<p>
+Le projet éducatif est un travail de réflexion collective qui permet d’échanger sur les pratiques mises en place à la crèche afin de construire une action pédagogique commune et atteindre des objectifs précis.
+</p>
+<p>Un projet pédagogique finalise les actions et les valeurs pédagogiques d’une équipe. Il donne du sens et de la cohérence aux actions éducatives pratiquées. C’est un outil pour se recentrer sur l’enfant. Il constitue les fondements de l’analyse des pratiques professionnelles dans le respect des enfants et des familles accueillies.
+</p>
+<p>C’est une référence commune pour l’équipe à laquelle chacun des membres de l’association adhère. Pour les familles, c’est le moyen de comprendre le travail de l’équipe et de le rendre accessible.
+</p>
+<a download target="_blank" href="https://drive.google.com/open?id=19FioIp8IXaWNA9XzEHyBaX6TlYaNlZtZ">Téléchargement</a>
+
+<h4>Les valeurs</h4>
+
+<h5>Bio</h5>
+
+<p>
+    Les repas sont constitués majoritairement d’aliments issus de l’agriculture biologique. La présence d’un cuisinier permet de disposer au quotidien de plats frais, sain et délicieux!
+</p>
+
+    <h5>Bienveillance</h5>
+<p>
+    Les décisions étant prises de manières démocratiques, une communication bienveillante entre les parents, les salariés et les enfants est primordiale afin de respecter la parole et les opinions de chacun.
+</p>
+
+        <h5>Convivialité</h5>
+
+
+        <p>C’est plus sympa de bien s’entendre!
+            Toutes les occasions sont donc bonnes de se retrouver à la crèche pour un barbecue, une soirée festive: Noël, vacances, printemps, …
+        </p>
+        <img src="/assets/images/fete3.png" width="100%"></img>
---
+++ pages/engagement-des-parents.php
@@ -0,0 +1,24 @@
+<h4>Une crèche parentale</h4>
+
+<p>L’implication personnelle des parents au quotidien est une spécificité de la crèche parentale. Si les parents sont avant tout les premiers éducateurs de leurs enfants, ils sont également encadrants, sous la responsabilité de l’éducateur de jeunes enfants ou de l'auxiliaire de puériculture, des autres enfants de la crèche pendant leurs temps de permanence. Chaque parent est donc impliqué, employeur et acteur.</p>
+
+<h4>Une association</h4>
+
+<p>
+La crèche Ty Bugale est une association loi 1901. Elle repose avant tout sur l’engagement et l’implication des parents tant au niveau de sa gestion que de son fonctionnement.
+</p>
+<p>
+Chaque famille accueillie régulièrement (plus de 3 jours par semaine)  fait partie du comité directeur. A ce titre, les familles se réunissent une fois par mois lors du Conseil d’Administration pour voter les grandes comme les petites orientations de la crèche. (chaque famille a un droit de vote).
+Une fois par an se tient une Assemblée Générale avec toutes les familles accueillies (accueils réguliers, occasionnels…)
+</p>
+
+<h4>Les postes</h4>
+
+<p>Chaque famille a un “poste” à pourvoir. Parmi ces postes, cinq postes font partie du bureau de l’association. Le bureau se réunit une fois par mois également, pour préparer le CA.</p>
+
+
+<h4>Les permanences</h4>
+
+<p>Chaque famille (accueillie 3 jours par semaine minimum à la crèche) effectue une “permanence” de 4h par semaine à la crèche. (en deça de trois jours d’accueil, la sollicitation est proportionnelle au temps d’accueil).</p>
+
+<p>Le bon fonctionnement de l’association est dépendant de la bonne tenue du poste et de la présence active des familles lors de leur permanence.</p>
