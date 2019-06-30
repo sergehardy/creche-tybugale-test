42f0b2de063ba4b6d4b7c16be357cca67df50b79
Serge HARDY <serge.hardy@zenika.com>
2019-06-30 21:21:11 +0200

    faq

---
+++ pages/faq.php
@@ -0,0 +1,118 @@
+<h4>La crèche</h4>
+
+<h5>Qu’est-ce qu’une crèche parentale?</h5>
+
+<p>
+    La particularité d'une crèche parentale tient au fait que l'organisme qui fonde puis gère administrativement cette
+    structure, est une association loi 1901 formée par les parents des enfants inscrits.
+</p>
+
+<p>
+    Généralement les crèches parentales emploient des professionnels de la petite enfance qui se chargent de l'accueil
+    de l'enfant à proprement parler ainsi que du contenu et de l'application du projet pédagogique.
+    La plupart des crèches parentales mettent également à contribution les parents de l'association pour effectuer des
+    heures de permanences afin d'épauler l'équipe pédagogique ainsi que pour se charger de quelques tâches matérielles
+    quotidiennes
+
+<p>
+    <em>Source: <a href="https://fr.wikipedia.org/wiki/Cr%C3%A8che_parentale">Wikipedia</a></em>
+</p>
+<h5>
+    Combien d'enfants peuvent être accueillis?</h5>
+
+<p>
+    21 enfants peuvent être accueillis à Ty Bugale. La répartition par tranche d’âge s’effectue en général de la manière
+    suivante:</p>
+<ul>
+    <li>3 bébés de 3 à 12 mois
+    <li>4 petits de 12 à 18 mois</li>
+    <li>7 moyens de 18 à 24 mois</li>
+    <li>7 grands de plus de 24 mois</li>
+</ul>
+
+<h5>Quels sont les horaires d'ouverture?</h5>
+
+<p>La crèche est ouverte de 8h à 18h30 du lundi au vendredi.
+    La crèche est fermée 4 semaines par an: 1 semaine à Noël, 3 semaines en août.
+
+<h5>Autre type d’accueil</h5>
+<p>Il est possible de demander:</p>
+
+<ul>
+    <li>
+        un accueil occasionnel
+    </li>
+    <li>
+        un accueil d’urgence, par exemple en cas d’assistante maternelle malade
+    </li>
+</ul>
+<p>Ces accueils sont réservés à des enfants âgés d’au moins 18 mois.
+</p>
+
+<h4>Le rôle des parents</h4>
+<h5>Quel est l'investissement à donner?</h5>
+
+<ul>
+    <li>assurer une permanence d’une demi-journée par semaine</li>
+    <li>prendre en charge un des postes réservés aux parents</li>
+    <li>prendre part aux conseils d’administrations une soirée par mois</li>
+</ul>
+
+<h5>Quelqu’un d’autre que les parents peut-il faire les permanences?</h5>
+
+<p>
+    Seuls les parents et beaux-parents de l’enfant sont autorisés à effectuer une permanence.
+</p>
+
+
+<h4>Inscription</h4>
+
+<h5>Est-ce que vous avez une place pour mon enfant?</h5>
+
+<p>Compte tenu du nombre important de demande, une liste d’attente a été mise en place. Une fois votre demande
+    effectuée, vous serez recontacté dès qu’une place de votre tranche d’âge se libère et que vous êtes premier sur la
+    liste.
+<h5>Je n’habite pas à Rennes, puis-je inscrire mon enfant?</h5>
+
+<p>Le financement de la crèche est assuré en partie par les cotisations des parents et en partie par des subventions
+    d’organismes publics : CAF, Département et Ville de Rennes.
+    A ce titre, seules les familles rennaises peuvent avoir une place à TyBugale.</p>
+<h5>Quel est le temps d'attente?</h5>
+
+<p>Ce temps est variable.
+    Lorsqu’une place se libère dans une tranche d'âge d'enfant, la place est proposée à la première famille de la liste
+    d'attente ce cette tranche. Si elle n'est pas intéressée, on propose à la suivante, etc...
+
+</p>
+
+<h5>Comment s'inscrire sur la liste d'attente?</h5>
+<p>
+    Les demandes d’inscription se font par mail à <a href="mailto:inscriptions@creche-tybugale.org">inscriptions@creche-tybugale.org</a>Vous
+    recevrez en retour une liste d’informations à fournir.
+
+</p>
+
+<h5>Lorsqu’une place se libère, est-on en concurrence pour la place avec d'autres familles?</h5>
+<p>Non, nous ne contactons qu'une famille à la fois en fonction de l’évolution de la liste d’attente.</p>
+
+<h5>Comment se déroule l'entretien?</h5>
+
+<p>Le cas échéant, vous serez invité pour une visite des locaux, une présentation de la crèche et un entretien avec un
+    parent de l'association et un salarié.
+</p>
+<p>Au cours de cet entretien seront évalués:</p>
+<ul>
+    <li>l’engagement des parents au sein de l’association</li>
+    <li>...</li>
+</ul>
+
+<p>
+    La décision définitive, prise en concertation avec le bureau de l’association, est notifiée à la famille dans la
+    semaine qui suit.
+</p>
+
+<h5>Combien ça coûte-t-il?</h5>
+<p>Cela dépend du quotient familial, les aides étant définies par la CAF. Plus d'informations <a
+            href="/prerequis">ici</a></p>
+
+
