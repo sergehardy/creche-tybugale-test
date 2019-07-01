cde809bf839cbd4232cc50d406ccfbbb978547f9
Serge HARDY <serge.hardy@zenika.com>
2019-07-01 21:43:27 +0200

    Nourriture

---
+++ pages/la-nourriture.php
@@ -0,0 +1,39 @@
+<h4>Une place centrale</h4>
+
+<img src="/assets/images/cuisine4.jpg" width="300px" class="float-left"></img>
+<p>
+    L'alimentation est un élément important dans la vie du nourrisson et du jeune enfant. Elle représente le centre de l'attention pour les parents et les adultes qui interviennent auprès des enfants ainsi qu'une source d'interactions sociales par la communication verbale et non verbale. L'expérience alimentaire apporte non seulement une valeur nutritive mais aussi des occasions d'apprentissages. Elle intervient dans la croissance et la santé physique des enfants ainsi que dans leur développement psycho-social et affectif. La relation alimentaire subit l'influence de la culture, de l'état de santé et des habitudes familiales.</p>
+
+<p>En collectivité, les repas et tout ce qui les entoure est affaire de tous.
+</p>
+
+<h4>Au jour le jour</h4>
+<p>La nourriture est l’un des piliers de la crèche car c’est un élément essentiel de l’évolution, de la santé et de l’éducation de nos enfants.
+</p>
+
+<p>C’est pourquoi Ty Bugale dispose d’un cuisinier qui élabore et prépare les repas sur place pour le plaisir des petits et des grands!</p>
+
+<p>En tant que parent vous pourrez en effet déguster les repas:</p>
+<ul>
+<li>en cas de permanence le matin, en compagnie des enfants et salariés</li>
+    <li>en fin de permanence l’après-midi, en ramenant chez vous des restes.</li>
+
+</ul>
+
+<h4>Charte</h4>
+
+<p>
+La crèche s’est dotée d’une charte permettant de cadrer son engagement en terme de nourriture.</p>
+<br>
+<table border="1">
+<tbody>
+<tr><th>Cuisine sur place</th><td>Le cuisinier prépare tous les jours sur place les plats en utilisant un maximum de produits frais.</td></tr>
+<tr><th>Bio</th><td>95% des produits sont issus de l’agriculture biologique.</td></tr>
+<tr><th>Local</th><td>Nous souhaitons privilégier les produits locaux, c’est une des ambitions pour 2019.</td></tr>
+<tr><th>Des menus équilibrés</th><td>Les menus sont conçus par le cuisinier de manières à équilibrer les différents apports nutritionnels.</td></tr>
+<tr><th>Végétarien</th><td>Actuellement 1 repas par semaine au moins est végétarien.</td></tr>
+<tr><th>Peu d’aliments transformés</th><td>Le cuisinier restreint autant que possible les aliments transformés. Les seuls
+aliments transformés qui sont utilisés sont : compotes, asperges, tomate, haricots, quelques achats congelés, épinards, choux-fleurs, haricots verts</td></tr>
+<tr><th>Gestion des allergies</th><td>Les menus sont faits par Matthieu pour que les enfants mangent tous la même chose et que les enfants avec allergies n’aient pas tout le temps l’impression d’être à part, ex. farine de petit épeautre sans gluten.</td></tr>
+</tbody>
+</table>
