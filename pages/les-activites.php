4d0a57a38fed7cbb03e73de6ce8ef05549c84820
Serge HARDY <serge.hardy@zenika.com>
2019-07-01 22:24:27 +0200

    Les activités

---
+++ pages/les-activites.php
@@ -0,0 +1,39 @@
+<p>Nous proposons des activités adaptées aux besoins et aux envies des enfants en fonction de leur développement. Le but
+    étant de prendre du plaisir en apprenant et en grandissant…
+</p>
+
+<p>Si les plus jeunes sont surtout dans des activités sensorimotrices de découvertes (transvasements, parcours moteurs,
+    éveil musical, manipulation de différentes matières, traces…), les plus grands accèdent petit à petit des activités
+    de plus en plus élaborées (construction, imaginaire, ateliers créations, collaboration…).
+</p>
+<h4>Les activités quotidiennes</h4>
+<p>Au moins une fois par jour, nous essayons de profiter des espaces extérieurs. Dans le jardin, il y a les amateurs de
+    tricycles ou de draisiennes, les constructeurs de pâtés de sable, les chercheurs de petites bêtes, ceux qui aiment
+    jardiner, ceux qui aiment rêvasser et regarder passer les oiseaux ou les avions, celui qui glisse sur le toboggan,
+    celui qui dessine à la craie, celui qui grimpe, celle qui court et bien d’autres encore…</p>
+
+<img src="/assets/images/motricite.png" class="float-left"></img>
+
+<p>La salle de motricité, c’est un peu le paradis ! Monter, descendre, sauter, grimper, s’allonger, nager dans la piscine...à balles… </p>
+
+<p>C’est un passage obligé dans la journée, que l’on sache courir, marcher, faire du quatre pattes, ramper ou rouler....
+</p>
+
+<h4>Les activités régulières</h4>
+<img src="/assets/images/peinture.png" class="float-left"></img>
+
+<p>
+Qu’as tu fait aujourd’hui à la crèche ? De la peinture sous les ongles ? De la farine dans les cheveux ? De la terre sur les bottes ? De la sueur sur le front ? Un body mouillé aux jeux d’eau ? Des lentilles de transvasement dans les chaussons ? Une chanson qui trotte dans la tête ? Un dessin à ramener à la maison ?
+</p>
+<h4>Les activités ponctuelles</h4>
+<img src="/assets/images/ferme.png" class="float-left"></img>
+<p>Une fois par semaine, un petit groupe d’enfants (les enfants sortent à tour de rôle) part en “sortie” ! Et c’est un peu l’événement ! Nous profitons des transports en commun (qui en soit sont déjà une sacré aventure) en bus, métro pour aller : à la bibliothèque (nous avons un partenariat avec la bibliothèque de Bréquigny), au marché, au parc, visiter le quartier, musée des beaux arts, la gare, l’aéroport… Nous sortons aussi parfois en vélo ou en triporteur !
+</p>
+<p>Deux fois par mois, nous accueillons Camille de l’association Terre des Arts, musicienne intervenante, qui anime deux ateliers pour petits et grands. Pendant 30 à 40 minutes, les enfants écoutent, chantent, miment, dansent, explorent les possibles du sonores…
+</p>
+
+<p>Nous aimons aussi travailler avec “L’arbre Yakafaire” et Marion qui vient nous raconter des histoires.
+</p>
+
+<p>Des temps forts rythment l’année : le carnaval, la fête de la musique, la semaine nationale de la petite enfance, les fêtes de fin d’année...
+</p>
