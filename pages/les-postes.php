2a035636f8d7222d151c6bdfd16de8545f018ea4
Serge HARDY <serge.hardy@zenika.com>
2019-07-01 22:13:20 +0200

    Les postes

---
+++ pages/les-postes.php
@@ -0,0 +1,104 @@
+<h4>Postes occupés par les parents</h4>
+
+<img src="/assets/images/parents2.jpg" width="200px" class="float-left">
+<p>Les familles dont l’enfant bénéficie d’un accueil régulier supérieur à 3 jours par semaine sont tenues d’occuper un
+    poste permettant d’assurer le bon fonctionnement de la crèche.</p>
+
+<p>Les familles dont l’enfant bénéficie d’un accueil régulier en deçà de 3 jours semaine pourront effectuer des missions
+    ponctuelles pour alléger certains postes ou en fonction des besoins.</p>
+
+
+<h4>Liste des postes</h4>
+
+<h5>Présidence</h5>
+
+<p>
+Le président de l’association en assure le bon fonctionnement au quotidien. Coordinateur et interlocuteur principal, il organise et anime notamment les conseils d’administrations mensuels et l’assemblée générale annuelle.  Il en est en outre le responsable juridique de l’association et la représente dans tous les actes civils.
+</p>
+<h5>Vice-Présidence</h5>
+
+<p>
+Le vice-président est le binôme du président. Il prend de plus part aux commissions et groupes de travail organisés par les parents, et est le principal interlocuteur des partenaires externes de la crèche.
+</p>
+<h5> Trésorier</h5>
+
+<p>
+Le trésorier assure la responsabilité des fonds de l’association. Il en est le référent auprès du comptable, verse les salaires et prélève les cotisations parentales. En fin d’exercice, il doit établir le bilan, le compte de résultats et le budget prévisionnel.
+</p>
+<h5>Référent salariés</h5>
+
+<p>
+Le référent salarié veille au bon déroulement du travail au sein de la crèche. A l’écoute des demandes des salariés, il gère les aspects contractuels, effectue les entretiens annuels et mène les recrutements éventuels.
+</p>
+<h5> Secrétariat</h5>
+
+<p>
+Le poste secrétaire assure le fonctionnement administratif de la structure en veillant au respect des règles de fonctionnement juridique de l’association. Moteur dans l’organisation des conseils d’administration et des assemblées générales, il en rédige les ordres du jour et les comptes-rendus.
+</p>
+<h5> Inscriptions</h5>
+
+<p>
+Le poste inscription est le point d’entrée pour les futurs arrivants à la crèche. Premier interlocuteur des familles, il leur présente la structure et son fonctionnement, les rencontre et effectue les démarches d’inscription. Il maintient à jour la liste d’attente des inscriptions.
+</p>
+<h5> Salaires</h5>
+
+<p>
+Le poste salaire traite les fiches de paies et les cotisations sociales en collaboration avec le cabinet comptable.
+</p>
+<h5> Remplacements</h5>
+
+<p>
+Les postes remplacements gèrent la continuité de la présence du personnel de la crèche et des parents de permanence de manière à garantir la qualité de l’accueil des enfants au quotidien.
+</p>
+<h5> Informatique</h5>
+
+<p>
+Le poste informatique est en charge du matériel, du site internet et de la gestion de la communication électronique: mails, documents partagés.
+</p>
+
+<h5>Bricolage</h5>
+<p>
+Le poste bricolage effectue les travaux de bricolage (réparations, installations diverses) à la demande des salariés et des parents, organise et suit les travaux importants, et effectue les achats ou commandes de matériel en rapport avec le poste.
+</p>
+<h5>Nutrition</h5>
+Le poste nutrition accompagne le cuisinier sur les questions d’équilibre des repas, de la gestion des stocks de denrées. Il élabore la charte nutrition et aide à sa mise en oeuvre.
+</p>
+<h5>Communication</h5>
+
+<p>
+Le poste communication est responsable de l’information à l’intérieur et à l’extérieur de la crèche. Il publie un périodique destiné aux parents (Ty Bugale info), gère les supports d’échanges d’information dans les locaux, et gère également les relations avec la presse.
+</p>
+<h5> Hygiène & Sécurité</h5>
+<p>
+Le poste hygiène et sécurité s’assure du bon respect des protocoles en vigueur à la crèche.. Il sensibilise les parents sur l’utilisation des produits de nettoyage et les soins apportés aux enfants. Il contrôle de plus les produits et matériel de nettoyage vis-à-vis des réglementations en cours .
+</p>
+<h5> Moyens Généraux</h5>
+<p>
+Le poste moyens généraux effectue les commandes hors alimentation, en assure le suivi et la livraison. Il recherche de nouveaux fournisseurs et optimise les achats.
+</p>
+<h5> Animations</h5>
+<p>
+Le poste animation organise des moments forts de la vie de la crèche:  pot de Noël, anniversaires, porte ouverte, barbecues..  Il propose également des idées de sortie sur Rennes pour les familles en week-end.
+</p>
+<h5> Formations</h5>
+<p>
+Le poste formation prend en charge le plan de formation des salariés conjointement avec le responsable d’équipe. Il propose également des formations aux parents.
+</p>
+<h5> Suivi ménage</h5>
+<p>
+Le poste suivi ménage est référent pour tous sujets liés à la propreté de la crèche en relation avec le salarié responsable du ménage. Il suit les fiches de postes des agents d’entretien, et sélectionne des produits et matériel de nettoyage.
+</p>
+<h5> Extérieurs</h5>
+<p>
+Le poste extérieur est responsable du jardinage et de l’outillage nécessaire de manière à garantir l’utilisation du jardin. Il gère de plus les interventions de professionnels.
+</p>
+<h5> Inventaire</h5>
+<p>
+Ce poste maintient à jour l’inventaire des stocks des produits nécessaire au fonctionnement quotidien (hygiène, ménage) et déclenche des commandes si nécessaire.
+</p>
+<h5> Temps pédagogique</h5>
+<p>
+Le temps pédagogique est un temps de réflexion et de partage à chaque début de conseil d’administration mensuel. Ce poste organise et anime ce temps en relation avec l’équipe, les parents et les éventuels intervenants extérieurs.
+</p>
+
+
