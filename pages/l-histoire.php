58f1284946f9175b8500946de89dec8aba66c471
Serge HARDY <serge.hardy@zenika.com>
2019-09-02 21:29:46 +0200

    htaccess et robots.txt

---
+++ pages/l-histoire.php
@@ -0,0 +1,14 @@
+<h4>Les origines</h4>
+<p>
+Fondée en 1986, la crèche Ty Bugale est née du souhait de parents de créer un mode garde qui leur correspondent, un mode de garde à taille humaine dont ils partageraient les valeurs et dans lequel ils pourraient s’investir.
+</p>
+<h4>Les lieux</h4>
+
+<p>
+Installée au 39, rue de l’Alma jusqu’en 2013, Ty Bugale va transiter pendant 4 ans dans des locaux provisoires boulevard Albert 1er, avant d’emménager en octobre 2017 au sein du quartier Mauconseil dans une maison entièrement neuve et aménagée par la ville de Rennes pour répondre aux besoins de la structure en terme fonctionnel, de capacité d’accueil et de normes de sécurité.
+</p>
+<p>
+    Nous disposons aujourd’hui de locaux adaptés à notre projet pédagogique, colorés, aérés et accueillants pour tous.
+</p>
+
+
